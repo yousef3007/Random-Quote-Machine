82fda0677a68fbd15df9f9804c5ff6fbacced775
Yousef <gegsbhxhx@gmail.com>
2022-09-12 00:01:47 +0200

    message

---
+++ index.js
@@ -0,0 +1,320 @@
+const quotes = [{
+    text: "I have just three things to teach: simplicity, patience, compassion. These three are your greatest treasures.",
+    source: "Lao Tzu"
+},
+{
+    text: "Do today what others won't and achieve tomorrow what others can't.",
+    source: "Jerry Rice"
+},
+{
+    text: "In character, in manner, in style, in all things, the supreme excellence is simplicity.",
+    source: "Henry Wadsworth Longfellow"
+},
+{
+    text: "If we don't discipline ourselves, the world will do it for us.",
+    source: "William Feather"
+},
+{
+    text: "Rule your mind or it will rule you.",
+    source: "Horace"
+},
+{
+    text: "Doing just a little bit during the time we have available puts you that much further ahead than if you took no action at all.",
+    source: "Pulsifer, Take Action; Don't Procrastinate"
+},
+{
+    text: "Never leave that till tomorrow which you can do today.",
+    source: "Benjamin Franklin"
+},
+{
+    text: "Procrastination is like a credit card: it's a lot of fun until you get the bill.",
+    source: "Christopher Parker"
+},
+{
+    text: "Someday is not a day of the week.",
+    source: "Author Unknown"
+},
+{
+    text: "Tomorrow is often the busiest day of the week.",
+    source: "Spanish Proverb"
+},
+{
+    text: "I can accept failure, everyone fails at something. But I can't accept not trying.",
+    source: "Michael Jordan"
+},
+{
+    text: "There’s a myth that time is money. In fact, time is more precious than money. It’s a nonrenewable resource. Once you’ve spent it, and if you’ve spent it badly, it’s gone forever.",
+    source: "Neil A. Fiore"
+},
+{
+    text: "Nothing can stop the man with the right mental attitude from achieving his goal; nothing on earth can help the man with the wrong mental attitude.",
+    source: "Thomas Jefferson"
+},
+{
+    text: "There is only one success--to be able to spend your life in your own way.",
+    source: "Christopher Morley"
+},
+{
+    text: "Success is the good fortune that comes from aspiration, desperation, perspiration and inspiration.",
+    source: "Evan Esar"
+},
+{
+    text: "We are still masters of our fate. We are still captains of our souls.",
+    source: "Winston Churchill"
+},
+{
+    text: "Our truest life is when we are in dreams awake.",
+    source: "Henry David Thoreau"
+},
+{
+    text: "The best way to make your dreams come true is to wake up.",
+    source: "Paul Valery"
+},
+{
+    text: "Life without endeavor is like entering a jewel mine and coming out with empty hands.",
+    source: "Japanese Proverb"
+},
+{
+    text: "Happiness does not consist in pastimes and amusements but in virtuous activities.",
+    source: "Aristotle"
+},
+{
+    text: "By constant self-discipline and self-control, you can develop greatness of character.",
+    source: "Grenville Kleiser"
+},
+{
+    text: "The difference between a successful person and others is not a lack of strength, not a lack of knowledge, but rather a lack in will.",
+    source: "Vince Lombardi Jr."
+},
+{
+    text: "At the end of the day, let there be no excuses, no explanations, no regrets.",
+    source: "Steve Maraboli"
+},
+{
+    text: "Inaction will cause a man to sink into the slough of despond and vanish without a trace.",
+    source: "Farley Mowat"
+},
+{
+    text: "True freedom is impossible without a mind made free by discipline.",
+    source: "Mortimer J. Adler"
+},
+{
+    text: "The most powerful control we can ever attain, is to be in control of ourselves.",
+    source: "Chris Page"
+},
+{
+    text: "Idleness is only the refuge of weak minds, and the holiday of fools.",
+    source: "Philip Dormer Stanhope"
+},
+{
+    text: "This is your life and it's ending one minute at a time.",
+    source: "Tyler Durden, Fight Club"
+},
+{
+    text: "You create opportunities by performing, not complaining.",
+    source: "Muriel Siebert"
+},
+{
+    text: "Great achievement is usually born of great sacrifice, and is never the result of selfishness.",
+    source: "Napoleon Hill"
+},
+{
+    text: "Whether you think you can, or you think you can't, you're right.",
+    source: "Henry Ford"
+},
+{
+    text: "Even if I knew that tomorrow the world would go to pieces, I would still plant my apple tree.",
+    source: "Martin Luther"
+},
+{
+    text: "Great acts are made up of small deeds.",
+    source: "Lao Tzu"
+},
+{
+    text: "The flame that burns Twice as bright burns half as long.",
+    source: "Lao Tzu"
+},
+{
+    text: "Perfection is achieved not when there is nothing more to add, but when there is nothing left to take away.",
+    source: "Antoine de Saint-Exupery"
+},
+{
+    text: "If you can't do great things, do small things in a great way.",
+    source: "Napoleon Hill"
+},
+{
+    text: "When I let go of what I am, I become what I might be.",
+    source: "Lao Tzu"
+},
+{
+    text: "Do not go where the path may lead, go instead where there is no path and leave a trail.",
+    source: "Ralph Waldo Emerson"
+},
+{
+    text: "Well done is better than well said.",
+    source: "Benjamin Franklin"
+},
+{
+    text: "Whatever you think the world is withholding from you, you are withholding from the world.",
+    source: "Ekhart Tolle"
+},
+{
+    text: "Muddy water is best cleared by leaving it alone.",
+    source: "Alan Watts"
+},
+{
+    text: "Do, or do not. There is no try.",
+    source: "Yoda"
+},
+{
+    text: "The superior man is modest in his speech, but exceeds in his actions.",
+    source: "Confucius"
+},
+{
+    text: "Optimism is the faith that leads to achievement. Nothing can be done without hope and confidence.",
+    source: "Helen Keller",
+},
+{
+    text: "We must believe that we are gifted for something, and that this thing, at whatever cost, must be attained.",
+    source: "Marie Curie"
+},
+{
+    text: "If you look at what you have in life, you’ll always have more. If you look at what you don’t have in life, you’ll never have enough.",
+    source: "Oprah Winfrey"
+},
+{
+    text: "You may encounter many defeats, but you must not be defeated. In fact, it may be necessary to encounter the defeats, so you can know who you are, what you can rise from, how you can still come out of it.",
+    source: "Maya Angelou"
+},
+{
+    text: "We need to start work with the idea that we’re going to learn every day. I learn, even at my position, every single day.",
+    source: "Chanda Kochhar"
+},
+{
+    text: "You can’t be that kid standing at the top of the waterslide, overthinking it. You have to go down the chute.",
+    source: "Tina Fey"
+},
+{
+    text: "Above all, be the heroine of your life, not the victim.",
+    source: "Nora Ephron"
+},
+{
+    text: "Learn from the mistakes of others. You can’t live long enough to make them all yourself.",
+    source: "Eleanor Roosevelt"
+},
+{
+    text: "What you do makes a difference, and you have to decide what kind of difference you want to make.",
+    source: "Jane Goodall"
+},
+{
+    text: "One of the secrets to staying young is to always do things you don’t know how to do, to keep learning.",
+    source: "Ruth Reichl"
+},
+{
+    text: "If you don’t risk anything, you risk even more.",
+    source: "Erica Jong"
+},
+{
+    text: "When the whole world is silent, even one voice becomes powerful.",
+    source: "Malala Yousafzai"
+},
+{
+    text: "The most common way people give up their power is by thinking they don’t have any.",
+    source: "Alice Walker"
+},
+{
+    text: "My philosophy is that not only are you responsible for your life, but doing the best at this moment puts you in the best place for the next moment.",
+    source: "Oprah Winfrey"
+},
+{
+    text: "Don’t be intimidated by what you don’t know. That can be your greatest strength and ensure that you do things differently from everyone else.",
+    source: "Sara Blakely"
+},
+{
+    text: "If I had to live my life again, I’d make the same mistakes, only sooner.",
+    source: "Tallulah Bankhead"
+},
+{
+    text: "Never limit yourself because of others’ limited imagination; never limit others because of your own limited imagination.",
+    source: "Mae C. Jemison"
+},
+{
+    text: "If you obey all the rules, you miss all the fun.",
+    source: "Katharine Hepburn"
+},
+{
+    text: "Life shrinks or expands in proportion to one’s courage.",
+    source: "Anaïs Nin"
+},
+{
+    text: "Avoiding danger is no safer in the long run than outright exposure. The fearful are caught as often as the bold.",
+    source: "Helen Keller"
+},
+{
+    text: "How wonderful it is that nobody need wait a single moment before beginning to improve the world.",
+    source: "Anne Frank"
+},
+{
+    text: "So often people are working hard at the wrong thing. Working on the right thing is probably more important than working hard.",
+    source: "Caterina Fake"
+},
+{
+    text: "Nothing is impossible; the word itself says ‘I’m possible’!",
+    source: "Audrey Hepburn"
+},
+{
+    text: "You only live once, but if you do it right, once is enough.",
+    source: "Mae West"
+}]
+function App(){
+ 
+    const [count, setCount] = React.useState(0);
+    const [change, setChange] = React.useState("#16a085");
+    function newQoute(){
+        let colors = [
+            '#16a085',
+            '#27ae60',
+            '#2c3e50',
+            '#f39c12',
+            '#e74c3c',
+            '#9b59b6',
+            '#FB6964',
+            '#342224',
+            '#472E32',
+            '#BDBB99',
+            '#77B1A9',
+            '#73A857'
+          ];
+        let randomQuote = Math.floor(Math.random() * quotes.length);
+        setCount(randomQuote);
+        let randomColer = Math.floor(Math.random() * colors.length);
+        setChange(colors[randomColer]);
+    }
+    return(
+        <div style={{
+            backgroundColor: change,
+            width: '100vw',
+            height: '100vh'
+          }}>
+            <div className ="square" id="quote-box">
+                <div className = "quote" id="text"> 
+                    "{quotes[count].text}"
+                </div>
+                <p className = "author" id="author">
+                    -{quotes[count].source}
+                </p>
+                <a href={`https://twitter.com/intent/tweet?hashtags=&related=&text="${quotes[count].qoute}"-${quotes[count].author}`}id="tweet-quote">
+                    <button>Tweet</button>
+                </a>
+                <button className="right" id="new-quote" onClick={newQoute}>
+                    New quote
+                </button>
+            </div>
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root')
+);
+root.render(<App />);
